d9c12b7bc6815d07eb643f0eef76fb55eec44e84
cristianbonomo <cristian.a.bonomo@gmail.com>
2020-05-27 00:51:50 -0300

    Updates

---
+++ precache-manifest.f113f54586182acf321bba631407d8ce.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db277b27ac7de1049a16b6ac1c1f4dfb",
+    "url": "/job-listings.git/index.html"
+  },
+  {
+    "revision": "d279f27a005527a8988f",
+    "url": "/job-listings.git/static/css/main.8e986d65.chunk.css"
+  },
+  {
+    "revision": "753bfef1a4dd90dee4a0",
+    "url": "/job-listings.git/static/js/2.2fc13a92.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/job-listings.git/static/js/2.2fc13a92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d279f27a005527a8988f",
+    "url": "/job-listings.git/static/js/main.6a090e4d.chunk.js"
+  },
+  {
+    "revision": "56d46b7f30b0dcc782fa",
+    "url": "/job-listings.git/static/js/runtime-main.84af9485.js"
+  },
+  {
+    "revision": "9f72c13bc10a76e4bcb8f7ab62582b9f",
+    "url": "/job-listings.git/static/media/account.9f72c13b.svg"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/job-listings.git/static/media/bg-header-desktop.6e2f7196.svg"
+  },
+  {
+    "revision": "6b5ff5a0e7f81b4c1247726c10c7aef5",
+    "url": "/job-listings.git/static/media/bg-header-mobile.6b5ff5a0.svg"
+  },
+  {
+    "revision": "39eaebdf1b82df9648392a9fb0915e9c",
+    "url": "/job-listings.git/static/media/eyecam-co.39eaebdf.svg"
+  },
+  {
+    "revision": "76faa79fba7225014706c0aac0b46472",
+    "url": "/job-listings.git/static/media/faceit.76faa79f.svg"
+  },
+  {
+    "revision": "ae67c678a143e6c6b913629f3d3b55e9",
+    "url": "/job-listings.git/static/media/icon-remove.ae67c678.svg"
+  },
+  {
+    "revision": "52b85a5018745489a50626498bc347ad",
+    "url": "/job-listings.git/static/media/insure.52b85a50.svg"
+  },
+  {
+    "revision": "6c50fd745d8d5c5e1dd47fa8c9029a6b",
+    "url": "/job-listings.git/static/media/loop-studios.6c50fd74.svg"
+  },
+  {
+    "revision": "fbe431a9e5d624447d698fee099ca843",
+    "url": "/job-listings.git/static/media/manage.fbe431a9.svg"
+  },
+  {
+    "revision": "e0ad9358d008658c14d4350d48627923",
+    "url": "/job-listings.git/static/media/myhome.e0ad9358.svg"
+  },
+  {
+    "revision": "3f34b49c96dcbb44450b43184cd6cd78",
+    "url": "/job-listings.git/static/media/photosnap.3f34b49c.svg"
+  },
+  {
+    "revision": "45f63772b29b7ccba3bc0c0c6e1d505d",
+    "url": "/job-listings.git/static/media/shortly.45f63772.svg"
+  },
+  {
+    "revision": "23b515f95978bcb24d704b1abecc8196",
+    "url": "/job-listings.git/static/media/the-air-filter-company.23b515f9.svg"
+  }
+]);
